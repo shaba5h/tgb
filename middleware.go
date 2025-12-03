8f6b66f5a2ee0c00078fa3b30d1031c64e8fba04
shaba5h <shaba5h+github@yandex.com>
2025-12-04 04:52:52 +0500

    feat: add `Recover` and `Logger` middlewares

--- middleware.go
+++ middleware.go
@@ -1,5 +1,12 @@
 package tgb
 
+import (
+	"fmt"
+	"log/slog"
+	"runtime/debug"
+	"time"
+)
+
 type Middleware func(next Handler) Handler
 
 type Middlewares []Middleware
@@ -9,4 +16,42 @@ func (m Middlewares) Apply(next Handler) Handler {
 		next = m[i](next)
 	}
 	return next
+}
+
+func Recover(next Handler) Handler {
+	return func(ctx *Context) (err error) {
+		defer func() {
+			if r := recover(); r != nil {
+				err = fmt.Errorf("panic: %v\n%s", r, debug.Stack())
+			}
+		}()
+		return next(ctx)
+	}
+}
+
+func Logger(logger *slog.Logger) Middleware {
+	return func(next Handler) Handler {
+		return func(ctx *Context) error {
+			start := time.Now()
+			err := next(ctx)
+
+			attrs := []any{
+				slog.String("duration", time.Since(start).String()),
+			}
+
+			if err != nil {
+				attrs = append(attrs, slog.String("error", err.Error()))
+			}
+
+			if user := ctx.User(); user != nil {
+				attrs = append(attrs,
+					slog.Int64("user_id", user.ID),
+					slog.String("username", user.Username),
+				)
+			}
+
+			logger.Info("update processed", attrs...)
+			return err
+		}
+	}
 }
