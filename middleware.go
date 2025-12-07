f3b4f3a6d3eba69c558232a7ba1f3b9777ab797e
shaba5h <shaba5h+github@yandex.com>
2025-12-07 05:51:50 +0500

    feat: inject logger in context in Logger middleware

--- middleware.go
+++ middleware.go
@@ -28,30 +28,3 @@ func Recover(next Handler) Handler {
 		return next(ctx)
 	}
 }
-
-func Logger(logger *slog.Logger) Middleware {
-	return func(next Handler) Handler {
-		return func(ctx *Context) error {
-			start := time.Now()
-			err := next(ctx)
-
-			attrs := []any{
-				slog.String("duration", time.Since(start).String()),
-			}
-
-			if err != nil {
-				attrs = append(attrs, slog.String("error", err.Error()))
-			}
-
-			if user := ctx.User(); user != nil {
-				attrs = append(attrs,
-					slog.Int64("user_id", user.ID),
-					slog.String("username", user.Username),
-				)
-			}
-
-			logger.Info("update processed", attrs...)
-			return err
-		}
-	}
-}
